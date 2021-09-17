e947f8325bba39b77cdbd9fc32039d204c44a5f8
Chandler <74741436+scottcd@users.noreply.github.com>
2021-09-16 20:31:38 -0400

    Add files via upload

--- AtmMachine/main.c
+++ AtmMachine/main.c
@@ -7,15 +7,38 @@ int main()
 {
 	char username[25];
 	char password[25];
-	printf("%s\n",loadAccounts());
+	loadAccounts();
 	
 	while(1)
 	{
+		printf("Welcome to the ATM machine.\n");
 		printf("Enter your username: ");
 		gets(username);
 		printf("Enter your password: ");
 		gets(password);
-		printf("%s\n", login(username, password));
+		int validation = login(username, password);
+
+		if(validation == 0)
+		{
+			continue;
+		}
+		
+		int menuChoice = 0;
+		
+		while(menuChoice != 4)
+		{
+			printf("Successful user login!\n");
+			printf("Please select an option below. (1-4)");
+			printf("1. View Balance");
+			printf("2. Deposit" );
+			printf("3. Withdrawal");
+			printf("4. Log Out");
+
+			int menuChoice = gets();
+			char atmResponse[100] = processSelection(menuChoice);
+		
+			printf("%s \n\n", atmResponse);
+		}
 	}
 
 	return 0;
